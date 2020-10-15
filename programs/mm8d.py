db80a765f20152fcec3ec42a716530d440e365a9
Pozsár Zsolt <pozsarzs@karak>
2020-10-16 00:23:15 +0200

    201015

--- programs/mm8d.py
+++ programs/mm8d.py
@@ -16,6 +16,8 @@
 # Exit codes:
 #   0: normal exit
 #   1: cannot open configuration file
+#  14: cannot open environment characteristic configuration file
+#  15: cannot create lock file
 
 import configparser
 import daemon
@@ -133,7 +135,140 @@ def addzero(num):
   return s
 
 # load environment characteristics
-# def loadenvirchars(channel,conffile):
+def loadenvirchars(channel,conffile):
+  global hheater_disable
+  global hheater_off
+  global hheater_on
+  global hhumidifier_disable
+  global hhumidifier_off
+  global hhumidifier_on
+  global hhumidity_max
+  global hhumidity_min
+  global hlight_off1
+  global hlight_off2
+  global hlight_on1
+  global hlight_on2
+  global htemperature_max
+  global htemperature_min
+  global hvent_disable
+  global hvent_disablelowtemp
+  global hvent_lowtemp
+  global hvent_off
+  global hvent_on
+  global mheater_disable
+  global mheater_off
+  global mheater_on
+  global mhumidifier_disable
+  global mhumidifier_off
+  global mhumidifier_on
+  global mhumidity_max
+  global mhumidity_min
+  global mlight_off1
+  global mlight_off2
+  global mlight_on1
+  global mlight_on2
+  global mtemperature_max
+  global mtemperature_min
+  global mvent_disable
+  global mvent_disablelowtemp
+  global mvent_lowtemp
+  global mvent_off
+  global mvent_on
+  H="hyphae"
+  M="mushroom"
+  hheater_disable=[[0 for x in range(24)] for x in range(16)]
+  hheater_off=[0 for x in range(16)]
+  hheater_on=[0 for x in range(16)]
+  hhumidifier_disable=[[0 for x in range(24)] for x in range(16)]
+  hhumidifier_off=[0 for x in range(16)]
+  hhumidifier_on=[0 for x in range(16)]
+  hhumidity_max=[0 for x in range(16)]
+  hhumidity_min=[0 for x in range(16)]
+  hlight_off1=[0 for x in range(16)]
+  hlight_off2=[0 for x in range(16)]
+  hlight_on1=[0 for x in range(16)]
+  hlight_on2=[0 for x in range(16)]
+  htemperature_max=[0 for x in range(16)]
+  htemperature_min=[0 for x in range(16)]
+  hvent_disable=[[0 for x in range(24)] for x in range(16)]
+  hvent_disablelowtemp=[[0 for x in range(24)] for x in range(16)]
+  hvent_lowtemp=[0 for x in range(16)]
+  hvent_off=[0 for x in range(16)]
+  hvent_on=[0 for x in range(16)]
+  mheater_disable=[[0 for x in range(24)] for x in range(16)]
+  mheater_off=[0 for x in range(16)]
+  mheater_on=[0 for x in range(16)]
+  mhumidifier_disable=[[0 for x in range(24)] for x in range(16)]
+  mhumidifier_off=[0 for x in range(16)]
+  humidifier_on=[0 for x in range(16)]
+  mhumidity_max=[0 for x in range(16)]
+  mhumidity_min=[0 for x in range(16)]
+  mlight_off1=[0 for x in range(16)]
+  mlight_off2=[0 for x in range(16)]
+  mlight_on1=[0 for x in range(16)]
+  mlight_on2=[0 for x in range(16)]
+  mtemperature_max=[0 for x in range(16)]
+  mtemperature_min=[0 for x in range(16)]
+  mvent_disable=[[0 for x in range(24)] for x in range(16)]
+  mvent_disablelowtemp=[[0 for x in range(24)] for x in range(16)]
+  mvent_lowtemp=[0 for x in range(16)]
+  mvent_off=[0 for x in range(16)]
+  mvent_on=[0 for x in range(16)]
+  try:
+    with open(conffile) as f:
+      envir_config=f.read()
+    config=configparser.RawConfigParser(allow_no_value=True)
+    config.read_file(io.StringIO(envir_config))
+    for x in range(24):
+      hhumidifier_disable[x][channel]=int(config.get(H,'humidifier_disable_'+addzero(x)))
+    for x in range(24):
+      hheater_disable[x][channel]=int(config.get(H,'heater_disable_'+addzero(x)))
+    for x in range(24):
+      hvent_disable[x][channel]=int(config.get(H,'vent_disable_'+addzero(x)))
+    for x in range(24):
+      hvent_disablelowtemp[x][channel]=int(config.get(H,'vent_disablelowtemp_'+addzero(x)))
+    hheater_off[channel]=int(config.get(H,'heater_off'))
+    hheater_on[channel]=int(config.get(H,'heater_on'))
+    hhumidifier_off[channel]=int(config.get(H,'humidifier_off'))
+    hhumidifier_on[channel]=int(config.get(H,'humidifier_on'))
+    hhumidity_max[channel]=int(config.get(H,'humidity_max'))
+    hhumidity_min[channel]=int(config.get(H,'humidity_min'))
+    hlight_off1[channel]=int(config.get(H,'light_off1'))
+    hlight_off2[channel]=int(config.get(H,'light_off2'))
+    hlight_on1[channel]=int(config.get(H,'light_on1'))
+    hlight_on2[channel]=int(config.get(H,'light_on2'))
+    htemperature_max[channel]=int(config.get(H,'temperature_max'))
+    htemperature_min[channel]=int(config.get(H,'temperature_min'))
+    hvent_lowtemp[channel]=int(config.get(H,'vent_lowtemp'))
+    hvent_off[channel]=int(config.get(H,'vent_off'))
+    hvent_on[channel]=int(config.get(H,'vent_on'))
+    for x in range(24):
+      mhumidifier_disable[x][channel]=int(config.get(M,'humidifier_disable_'+addzero(x)))
+    for x in range(24):
+      mheater_disable[x][channel]=int(config.get(M,'heater_disable_'+addzero(x)))
+    for x in range(24):
+      mvent_disable[x][channel]=int(config.get(M,'vent_disable_'+addzero(x)))
+    for x in range(24):
+      mvent_disablelowtemp[x][channel]=int(config.get(M,'vent_disablelowtemp_'+addzero(x)))
+    mheater_off[channel]=int(config.get(M,'heater_off'))
+    mheater_on[channel]=int(config.get(M,'heater_on'))
+    mhumidifier_off[channel]=int(config.get(M,'humidifier_off'))
+    mhumidifier_on[channel]=int(config.get(M,'humidifier_on'))
+    mhumidity_max[channel]=int(config.get(M,'humidity_max'))
+    mhumidity_min[channel]=int(config.get(M,'humidity_min'))
+    mlight_off1[channel]=int(config.get(M,'light_off1'))
+    mlight_off2[channel]=int(config.get(M,'light_off2'))
+    mlight_on1[channel]=int(config.get(M,'light_on1'))
+    mlight_on2[channel]=int(config.get(M,'light_on2'))
+    mtemperature_max[channel]=int(config.get(M,'temperature_max'))
+    mtemperature_min[channel]=int(config.get(M,'temperature_min'))
+    mvent_lowtemp[channel]=int(config.get(M,'vent_lowtemp'))
+    mvent_off[channel]=int(config.get(M,'vent_off'))
+    mvent_on[channel]=int(config.get(M,'vent_on'))
+    writetodebuglog("i","Environment characteristics is loaded.")
+  except:
+    writetodebuglog("e","Cannot open "+conffile+"!")
+    exit(14)
 
 # create and remove lock file
 def lckfile(mode):
@@ -182,8 +317,49 @@ def writelog(channel,temperature,humidity,gasconcentrate,statusdata):
   except:
     writetodebuglog("e","Cannot write "+logfile+"!")
     lckfile(0)
-    exit(3)
+    exit(15)
   lckfile(0)
+
+# check external control files
+def extcont(channel,output,status):
+  writetodebuglog("i","Checking override file: "+dir_var+addzero(channel)+"/out"+str(output)+".")
+  if os.path.isfile(dir_var+addzero(channel)+"/out"+str(output)):
+    try:
+      f=open(dir_var+addzero(channel)+"/out"+str(output),'r')
+      v=f.read()
+      f.close()
+      if v == "neutral": s=status
+      if v == "off": s="0"
+      if v == "on": s="1"
+    except:
+      s=status
+  else:
+    s=status
+  return s
+
+# blink ACTIVE LED
+def blinkactled():
+  GPIO.output(prt_act,1)
+  time.sleep(0.5)
+  GPIO.output(prt_act,0)
+  time.sleep(0.5)
+
+# get external temperature from openweathermap.org
+def getexttemp():
+  writetodebuglog("i","Get external temperature from internet.")
+  response=requests.get(base_url+"appid="+api_key+"&q="+city_name)
+  x=response.json()
+  if x["cod"]!="404":
+    y=x["main"] 
+    current_temperature=y["temp"]
+    current_temperature=round(current_temperature-273)
+    writetodebuglog("i","External temperature: "+str(current_temperature)+" degree Celsius")
+    return current_temperature
+  else:
+    writetodebuglog("w","Cannot get external temperature from internet.")
+    return 18
+
+
 
 
 
